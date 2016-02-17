afe9b98c03a10a013ac1ea6cc576d2b723ead9bf
Mathcar <mattec_93@hotmail.com>
2016-02-17 14:07:56 +0100

    moved the intitialazation of the view from setUpGame to Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		AppGameContainer app;
+		try {
+			app = new AppGameContainer(new SetUpClass("SetUpTest"));
+			
+			app.setDisplayMode(640, 480, false);
+			app.setShowFPS(true);
+			app.setAlwaysRender(true);
+			app.setVSync(true);
+			app.setMaximumLogicUpdateInterval(60);
+			app.start(); 
+			
+		} catch (SlickException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 
 	}
 
