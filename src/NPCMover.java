99494642bde3009d9442ff870cd201c86f4df459
Xaeoz <hampf94@gmail.com>
2016-02-18 16:16:23 +0100

    Pathfinding added
    Very simple pathfinding along with some small testing functionality has
    been added. Testing stuff has been encapsuled in comments in the
    SetUpClass code

---
+++ src/NPCMover.java
@@ -0,0 +1,43 @@
+import org.newdawn.slick.util.pathfinding.Mover;
+import org.newdawn.slick.util.pathfinding.Path;
+
+/**
+ * Created by Hampus on 2016-02-18.
+ * A very basic template class which contains the things needed to utilize the inherent pathfinding
+ *
+ */
+public class NPCMover implements Mover {
+    public int x,y;
+    private Path currentPath;
+    private int pathCounter = 0;
+
+    public NPCMover(int startX, int startY){
+        x = startX;
+        y = startY;
+    }
+
+    //Small template on an update method regarding movement along a path
+    public void update(){
+        //Move along the current path if there is one
+        if(currentPath!=null) {
+            int targetX = currentPath.getStep(pathCounter).getX()*32;
+            int targetY = currentPath.getStep(pathCounter).getY()*32;
+            if (targetX != x) {
+                x += (targetX - x) / Math.abs(targetX - x); //Move one pixel per update in the correct x direction
+            } else if (targetY != y) {
+                y += (targetY - y) / Math.abs(targetY - y); //Move one pixel per update in the correct x direction
+            } else if (currentPath.getLength() > pathCounter+1) {
+                pathCounter++;  //If a grid has been reached, increment the path so the next grid will be the target
+            } else{
+                currentPath = null; //Remove current path if destination has been reached
+            }
+        }
+    }
+
+    public void addPath(Path p){
+        pathCounter = 0;
+        currentPath = p;
+    }
+
+
+}
--- src/SetUpClass.java
+++ src/SetUpClass.java
@@ -6,12 +6,22 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
+import org.newdawn.slick.util.pathfinding.AStarPathFinder;
+import org.newdawn.slick.util.pathfinding.Path;
 
 public class SetUpClass extends BasicGame {
 
 	private TiledMap map1, map2;
 	private int roomNum = 1;
-	
+	// A*pathfinding testing variables-------------
+	private AStarPathFinder pathFinder;
+	private LayerBasedTileMap pathfindingMap;
+	private int currentX = 0;
+	private int currentY = 0;
+	private int currentLayer = 0;
+	private NPCMover testNPC;
+	//-------------------------------------------
+
 	public SetUpClass(String title) {
 		super(title);
 	}
@@ -24,22 +34,54 @@ public class SetUpClass extends BasicGame {
 		else if(roomNum == 2){
 			map2.render(0, 0);
 		}
-		
+
+        //----------PATHFINDING TEST RENDERS------------------------
+        //drawGrid(arg1);
+		//arg1.drawString("X=" + currentX + "   Y=" + currentY + "    Layer=" + currentLayer + "     ID =" + map1.getTileId(currentX,currentY,currentLayer), 100,100);
+        arg1.drawOval(testNPC.x, testNPC.y, 20,20);
+        //---------------------------------------------------------
 	}
 
 	public void init(GameContainer container) throws SlickException {
 		map1 = new TiledMap("res/pictures/living_room2.tmx");
 		map2 = new TiledMap("res/pictures/kitchen.tmx");
+
+        //Pathfinding test inits-----------------------------
+		pathfindingMap = new LayerBasedTileMap(map1);
+		pathFinder = new AStarPathFinder(pathfindingMap, 100, false);
+		testNPC = new NPCMover(64,64);
+        //----------------------------------------------------
+
 	}
 
 	public void update(GameContainer container, int delta) throws SlickException {
-		Input input = container.getInput();
+		testNPC.update();
+
+        Input input = container.getInput();
 		if(input.isKeyPressed(Input.KEY_1)){
 			roomNum = 1;
 		}else if (input.isKeyPressed(Input.KEY_2)){
 			roomNum = 2;
 		}
-		
+        //PATHFINDING TEST INPUTS-----------------------------------------
+        if(input.isMousePressed(0)) {
+            System.out.println("mouse is at " + input.getMouseX() / 32 + "   " + input.getMouseY() / 32);
+            testNPC.addPath(pathFinder.findPath(testNPC, testNPC.x / 32, testNPC.y / 32, input.getMouseX() / 32, input.getMouseY() / 32));
+            System.out.println("Path made");
+        }
+        //-----------------------------------------------------------------
 	}
+
+
+    //Draws a grid over the background TiledMap, currently requires origin of (0,0)
+    public void drawGrid(Graphics g){
+        for(int i = 0; i<pathfindingMap.getWidthInTiles(); i++){
+            for(int j=0; j<pathfindingMap.getHeightInTiles();j++){
+                g.drawRect(i*32,j*32,32,32);
+            }
+
+        }
+
+    }
 
 }
