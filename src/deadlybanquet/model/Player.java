7e88593dab9e79e8233d71d021e3131dd800fc6a
Mathcar <mattec_93@hotmail.com>
2016-03-08 14:23:41 +0100

    implemented simple input in player

--- src/deadlybanquet/model/Player.java
+++ src/deadlybanquet/model/Player.java
@@ -6,19 +6,29 @@ public class Player {
 	
 	private Character character;
 	
+	private final static int MOVEMNET_DELAY = 500;
+	private int movmentTimer = 0;
+	
 	public Player(){
-		
+		this.character = new Character("Gandalf", 5, 5);
+	}
+	
+	public Player(Character c){
+		this.character = c;
 	}
 	
 	public void uppdate(Input in, int dt){
-		if(in.isKeyPressed(Input.KEY_UP)){
-			character.moveN();
-		}else if(in.isKeyPressed(Input.KEY_DOWN)){
-			character.moveS();
-		}else if(in.isKeyPressed(Input.KEY_LEFT)){
-			character.moveW();
-		}else if(in.isKeyPressed(Input.KEY_RIGHT)){
-			character.moveE();
+		if(this.movmentTimer < 1){
+			if(in.isKeyPressed(Input.KEY_UP)){
+				character.moveN();
+				movmentTimer = MOVEMNET_DELAY;
+			}else if(in.isKeyPressed(Input.KEY_DOWN)){
+				character.moveS();
+			}else if(in.isKeyPressed(Input.KEY_LEFT)){
+				character.moveW();
+			}else if(in.isKeyPressed(Input.KEY_RIGHT)){
+				character.moveE();
+			}
 		}
 	}
 	
