09c1baa83d29f48324362a64f9c7979c5611fa74
Omega <karin.wibergh@gmail.com>
2016-03-09 14:47:58 +0100

    Added stubs for getting information from a speech act and for creating speech acts

--- src/deadlybanquet/speech/SpeechAct.java
+++ src/deadlybanquet/speech/SpeechAct.java
@@ -2,6 +2,8 @@ package deadlybanquet.speech;
 
 import java.util.List;
 import java.util.Map;
+import deadlybanquet.ai.IThought;
+import java.util.ArrayList;
 
 /**
  * Created by Tom on 2016-02-25.
@@ -9,6 +11,12 @@ import java.util.Map;
 public interface SpeechAct {
 
     public String getText();
+    
+    //gets the information content of the speech act.
+    public ArrayList<IThought> getContent();
+    
+    //Gets the identifier of the speaker.
+    public String getSpeaker();
 
     public boolean isEndConversation();
 
