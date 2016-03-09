a9fe036ff16448df3654da34ec9e9487a6c7c249
Omega <karin.wibergh@gmail.com>
2016-03-09 15:31:01 +0100

    Changed name of AI.Position to Whereabouts. Also added method stubs to Brain.

--- src/deadlybanquet/ai/PlanElement.java
+++ src/deadlybanquet/ai/PlanElement.java
@@ -1,8 +1,16 @@
 package deadlybanquet.ai;
 
 /**
- * This may or may not be a useful instance of IThought.
+ * 
  * The main purpose of this class is in formulating plans inside the AI.
+ * In a speech act, this may be interpreted as "Ask something" (if the value of action is ASK)
+ * give information etc. The intended information to be requested or given is found 
+ * in the results list. The prerequsites act as a because clause 
+ * I want to know X, because I don't know it
+ * can be represented as
+ * prerequisite = don't know X
+ * action = ASK
+ * results = know X
  * @author omega
  */
 public class PlanElement implements IThought {
