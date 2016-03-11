d4efe912fd2fd4a11bf848c95e287f11c48ffc13
tomd-chalmers <tom.a.dahlen@gmail.com>
2016-03-11 11:16:56 +0100

    This dose NOT work, but should work with karins push.

--- src/deadlybanquet/model/Room.java
+++ src/deadlybanquet/model/Room.java
@@ -66,7 +66,7 @@ public class Room implements TileBasedMap {
         Position newPos = c.getFacedTilePos();
         if(isBlocked(newPos.getX(), newPos.getY())){
             //tile is blocked, send notification to related ai/character?
-            c.notifyBlocked();
+            //c.notifyBlocked();
         }else{
             c.move(); //character can move
         }
--- src/deadlybanquet/speech/ReadFile.java
+++ src/deadlybanquet/speech/ReadFile.java
@@ -1,22 +1,31 @@
 package deadlybanquet.speech;
 
+import deadlybanquet.ai.Action;
+import deadlybanquet.ai.IThought;
+import deadlybanquet.ai.PlanElement;
+import javafx.util.Pair;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Tom on 2016-03-11.
  */
 public class ReadFile {
     private static ReadFile instance = null;
+
+    private HashMap<String,Pair<TextPropertyEnum,ArrayList<IThought>>> greetingsList;
+
     public ReadFile(){
-        //here the reading of the files
 
     }
 
-    public void readFile(File file){
+    public HashMap<String,Pair<TextPropertyEnum,ArrayList<IThought>>> readFile(File file){
+        HashMap temp = new HashMap<String,Pair<TextPropertyEnum,ArrayList<IThought>>>();
         BufferedReader br = null;
         String line = "";
         try{
@@ -28,8 +37,30 @@ public class ReadFile {
                     list.add(i,div[i]);
                 }
                 // Now list is a list of each "thing" in each line.
+                TextPropertyEnum textEnum=TextPropertyEnum.NUTRAL;
+                try{
+                    textEnum = TextPropertyEnum.valueOf(list.get(1));
+                }catch(Exception e){
+                    System.err.println("Something is wrong: could not make string to TextPropery enum[ReadFile class]" +
+                            "\nThe enum is set to NEUTRAL");
+                }
 
-                //todo
+                ArrayList<IThought> iThoughts= new ArrayList<IThought>();
+                for(int i = 2;i<list.size();i++){
+                    IThought iThought;
+                    try{
+                        iThought=new PlanElement(null, Action.valueOf(list.get(i)),null);
+                        iThoughts.add(iThought);
+                    }catch(Exception e){
+                        System.err.println("Something is wrong: could not make string to Action enum[ReadFile class]" +
+                                "\nThe program has to close here");
+                        System.exit(3);
+                    }
+                }
+
+
+                temp.put(list.get(0),new Pair(textEnum,iThoughts));
+                // This will make each line in the text files will be matched to one "thing" i the HashMap
             }
         }catch (IOException e){
             System.err.println("IOException while reading file");
@@ -46,6 +77,8 @@ public class ReadFile {
                 }
             }
         }
+
+        return temp;
     }
 
     public static ReadFile getInstance(){
