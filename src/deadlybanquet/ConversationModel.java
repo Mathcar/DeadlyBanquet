1fd947bbeb34fdf0a1e2122d8e6c6ae0e975948f
tomd-chalmers <tom.a.dahlen@gmail.com>
2016-03-30 10:36:38 +0200

    a quick ConversationModel test

---
+++ src/deadlybanquet/ConversationModel.java
@@ -0,0 +1,35 @@
+package deadlybanquet;
+
+import deadlybanquet.ai.Action;
+import deadlybanquet.model.NPC;
+import deadlybanquet.model.Player;
+import deadlybanquet.speech.SpeechAct;
+
+/**
+ * Created by Tom on 2016-03-30.
+ */
+public class ConversationModel {
+
+    // for now only works with players and NPC
+    Player player;
+    NPC npc;
+    public ConversationModel(Player player, NPC npc){
+        this.player=player;
+        this.npc=npc;
+    }
+
+    private void conversation(){
+        // for now assume that player will start to speak
+        boolean end=false;
+        /*while(!end){
+            SpeechAct p = player.saySomeThing();
+            npc.getBrain.hear(p);
+            SpeechAct n = npc.saySomeThing();
+            player.hear(n);
+            if(p.getContent().contains(Action.GOODBYE) || n.getContent().contains(Action.GOODBYE)){
+                end=true;
+            }
+        }*/
+    }
+
+}
