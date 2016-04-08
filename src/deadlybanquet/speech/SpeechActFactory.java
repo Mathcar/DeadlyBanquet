8dd721ea1d2d7b43e0e136c308e385bcd50c28e6
tomd-chalmers <tom.a.dahlen@gmail.com>
2016-04-08 16:14:40 +0200

    added speechact2 for tests.

--- src/deadlybanquet/speech/SpeechActFactory.java
+++ src/deadlybanquet/speech/SpeechActFactory.java
@@ -2,6 +2,7 @@ package deadlybanquet.speech;
 
 
 import deadlybanquet.Talkable;
+import deadlybanquet.ai.IPerceiver;
 import deadlybanquet.ai.NPCBrain;
 import deadlybanquet.ai.IThought;
 import deadlybanquet.model.Player;
@@ -27,24 +28,78 @@ public class SpeechActFactory {
         return null;
     }
 
+    IPerceiver A;
+    IPerceiver B;
+
+    SpeechAct lastThingSaid;
+
+    IPerceiver talkingRightNow;
+
+    public SpeechActFactory(IPerceiver A, IPerceiver B){
+        this.A=A;
+        this.B=B;
+    }
+
+    public SpeechAct2 generateSpeechAct(IPerceiver talker, ArrayList<IThought> listOfIThought){
+        this.talkingRightNow=talker;
+        SpeechAct2 temp;
+        //blabla generate a speechAct her
+        temp = new SpeechAct2("hello there #name");
+        //now parse the SpeechAct
+        String line=temp.getLine();
+        if(wordsToBeParsed(line)!=null){
+            line=lineParser(line,wordsToBeParsed(line));
+        }
+        temp.setLine(line);
+        return temp;
+    }
+
+
+    private String lineParser(String line,String word){
+        String temp="ERROR VALUE";
+        switch(word){
+            case "#name":   temp=caseName(line);
+                            break;
+            case "#person": temp=casePerson(line);
+                            break;
+            case "#location":temp=caseLocation(line);
+                            break;
+            default:        System.err.println("Error in SpeechActFactory, could not recognize the placeholder String");
+                            break;
+        }
+        return temp;
+    }
+
+    private String caseName(String line){
+        //talkingRightNow
+        return "NameNotImplemented";
+    }
+
+    private String caseLocation(String line){
+        return "LocationNotImplemented";
+    }
+
+    private String casePerson(String line){
+        return "PersonNameNotImplemented";
+    }
+
 
     /*
-    Returns all the words/names/whatever, that needs to be changed or parsed.
-    If the list is empty, no words needs to be parsed and can be directly
-    transmitted to the screen.
+    Returns the word that are needed to be parsed, it there is no word that are needed
+    return NULL OBS!!! probably change this later.
      */
-    private ArrayList<String> wordsToBeParsed(String line){
-        ArrayList<String> temp = new ArrayList<>();
+    private String wordsToBeParsed(String line){
+        String temp = "";
         if(line.contains("#")){
             String[] list = line.split(" ");
             for(int i=0;i<list.length;i++){
                 if(list[i].contains("#")){
-                    temp.add(list[i]);
+                    temp=list[i];
                 }
             }
             return temp;
         }else{
-            return temp;
+            return null;
         }
     }
 
