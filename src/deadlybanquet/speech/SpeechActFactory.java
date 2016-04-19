338fd6785c30453f7dc27f3e1e8ae77fc8bdbe58
tomd-chalmers <tom.a.dahlen@gmail.com>
2016-04-19 15:44:20 +0200

    added simple parser

--- src/deadlybanquet/speech/SpeechActFactory.java
+++ src/deadlybanquet/speech/SpeechActFactory.java
@@ -54,68 +54,41 @@ public class SpeechActFactory {
         }
     }
 
-    public SpeechAct generateSpeechAct(IPerceiver talker, ArrayList<IThought> listOfIThought){
-        this.talker=talker;
-        SpeechAct temp;
-        //blabla generate a speechAct her
-        temp = new SpeechAct("hello there #name");
-        //now parse the SpeechAct
-        String line=temp.getLine();
-        if(wordsToBeParsed(line)!=null){
-            line=lineParser(line,wordsToBeParsed(line));
-        }
-        temp.setLine(line);
-        return temp;
-    }
 
-
-    private String lineParser(String line,String word){
-        String temp="ERROR VALUE";
-        switch(word){
-            case "#name":   temp=caseName(line);
-                            break;
-            case "#person": temp=casePerson(line);
-                            break;
-            case "#location":temp=caseLocation(line);
-                            break;
-            default:        System.err.println("Error in SpeechActFactory, could not recognize the placeholder String");
-                            break;
+    private String parseSpeechAct(String text){
+        String temp = text;
+        if(temp.contains("#name")){
+            temp = temp.replace("#name",getListener().getName());
         }
         return temp;
     }
 
-    private String caseName(String line){
-        //talkingRightNow
-        return "NameNotImplemented";
-    }
-
-    private String caseLocation(String line){
-        return "LocationNotImplemented";
-    }
-
-    private String casePerson(String line){
-        return "PersonNameNotImplemented";
-    }
-
-
-    /*
-    Returns the word that are needed to be parsed, it there is no word that are needed
-    return NULL OBS!!! probably change this later.
-     */
-    private String wordsToBeParsed(String line){
-        String temp = "";
-        if(line.contains("#")){
-            String[] list = line.split(" ");
-            for(int i=0;i<list.length;i++){
-                if(list[i].contains("#")){
-                    temp=list[i];
-                }
-            }
-            return temp;
-        }else{
-            return null;
+    private String parseSpeechAct(String text, Whereabouts w){
+        String temp=text;
+        if(temp.contains("#name")){
+            temp = temp.replace("#name",getListener().getName());
         }
+        if(temp.contains("#location")){
+            temp = temp.replace("#location",w.getRoom());
+        }
+        if(temp.contains("#person")){
+            temp = temp.replace("#person",w.getCharacter());
+        }
+        return temp;
     }
+
+    private String parseSpeechAct(String text, Opinion o){
+        String temp = text;
+        if(temp.contains("#name")){
+            temp = temp.replace("#name",getListener().getName());
+        }
+        if(temp.contains("#person")){
+            temp = temp.replace("#person",o.getPerson());
+        }
+        return temp;
+    }
+
+
 
     public SpeechAct convertIThoughtToSpeechAct(IThought i, TextPropertyEnum prop){
         ArrayList<IThought> IThoughtList = new ArrayList<>();
@@ -131,6 +104,7 @@ public class SpeechActFactory {
                         text=list.get(k).getText();
                     }
                 }
+                text=parseSpeechAct(text);
                 temp = new SpeechAct(text,talker.getName(),getListener().getName(),SpeechType.GREET,prop,IThoughtList);
             }
             else if(i.equals(BeingPolite.GOODBYE)) {
@@ -141,6 +115,7 @@ public class SpeechActFactory {
                         text = list.get(k).getText();
                     }
                 }
+                text=parseSpeechAct(text);
                 temp = new SpeechAct(text, talker.getName(), getListener().getName(), SpeechType.GOODBYE, prop,IThoughtList);
             }
             else{
@@ -160,6 +135,7 @@ public class SpeechActFactory {
                             break;
                         }
                     }
+                    text=parseSpeechAct(text,(Whereabouts) i);
                     temp = new SpeechAct(text,talker.getName(),getListener().getName(),SpeechType.WHERE_LOCATION,prop,IThoughtList);
                 }else if(((Whereabouts) i).getRoom().equals("")){
                     // where is getCharacter()
@@ -170,6 +146,7 @@ public class SpeechActFactory {
                             break;
                         }
                     }
+                    text=parseSpeechAct(text,(Whereabouts) i);
                     temp = new SpeechAct(text,talker.getName(),getListener().getName(),SpeechType.WHERE_PERSON,prop,IThoughtList);
                 }
             }else{ // not a question!
@@ -182,6 +159,7 @@ public class SpeechActFactory {
                         break;
                     }
                 }
+                text=parseSpeechAct(text,(Whereabouts) i);
                 temp = new SpeechAct(text,talker.getName(),getListener().getName(),SpeechType.INFO_P_LOCATION,prop,IThoughtList);
             }
         }else if(i instanceof Opinion){
@@ -195,6 +173,7 @@ public class SpeechActFactory {
                         break;
                     }
                 }
+                text=parseSpeechAct(text,(Opinion) i);
                 temp = new SpeechAct(text,talker.getName(),getListener().getName(),SpeechType.OPINION_QUESTION,prop,IThoughtList);
             }else{
                 //info on opinion on someone.
@@ -216,7 +195,7 @@ public class SpeechActFactory {
     NOT DONE!
     todo: Check ConversationModel Diagram.
      */
-    public ArrayList<SpeechAct> getDialogueOptions(TextPropertyEnum prop){
+    public ArrayList<SpeechAct> getDialogueOptions(TextPropertyEnum prop,Boolean first){
         ArrayList<SpeechAct> temp = new ArrayList<>();
 
         //add first thing
@@ -224,11 +203,16 @@ public class SpeechActFactory {
         Whereabouts w = new Whereabouts("Tom","hell");
         Opinion o = new Opinion("Tom",new PAD(0.0,0.0,0.0));
 
-        temp.add(convertIThoughtToSpeechAct(i,prop));
-        temp.add(convertIThoughtToSpeechAct(w,prop));
-        temp.add(convertIThoughtToSpeechAct(o,prop));
-        temp.add(convertIThoughtToSpeechAct(BeingPolite.GREET,prop));
-        temp.add(convertIThoughtToSpeechAct(BeingPolite.GOODBYE,prop));
+        if(first==true){
+            temp.add(convertIThoughtToSpeechAct(BeingPolite.GREET,prop));
+        }else{
+            temp.add(convertIThoughtToSpeechAct(i,prop));
+            temp.add(convertIThoughtToSpeechAct(w,prop));
+            temp.add(convertIThoughtToSpeechAct(o,prop));
+            temp.add(convertIThoughtToSpeechAct(BeingPolite.GOODBYE,prop));
+        }
+
+
 
         return temp;
     }
