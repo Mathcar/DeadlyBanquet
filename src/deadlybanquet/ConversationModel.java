d84e4edfb42a8446dd95f8815d904f7e9728aa4e
Xaeoz <hampf94@gmail.com>
2016-04-19 17:05:15 +0200

    Updated the conversationModel

--- src/deadlybanquet/ConversationModel.java
+++ src/deadlybanquet/ConversationModel.java
@@ -1,5 +1,6 @@
 package deadlybanquet;
 
+import deadlybanquet.speech.SpeechActFactory;
 import org.newdawn.slick.Image;
 
 import deadlybanquet.ai.BeingPolite;
@@ -9,23 +10,40 @@ import deadlybanquet.model.NPC;
 import deadlybanquet.model.Player;
 import deadlybanquet.speech.SpeechAct;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Tom on 2016-03-30.
  */
 public class ConversationModel {
 
     // for now only works with players and NPC
-    IPerceiver p1, p2;
-    Image p,n;
+    private IPerceiver perceiver1, perceiver2;
+    private Image playerImage,npcImage;
+    private SpeechActFactory saFactory;
+    //Used to record the conversation
+    private HashMap<IPerceiver, ArrayList<SpeechAct>> actHistory;
+
+    //Constructor needed when player is a part of the conversation
     public ConversationModel(IPerceiver player, IPerceiver npc, Image playerImage, Image npcImage){
-        this.p1=player;
-        this.p2=npc;
-        this.p=playerImage;
-        this.n=npcImage;
+        this.perceiver1=player;
+        this.perceiver2=npc;
+        this.playerImage=playerImage;
+        this.npcImage=npcImage;
     }
+
+    //Constructor for a conversation inbetween NPCs
     public ConversationModel(IPerceiver player, IPerceiver npc){
-        this.p1=player;
-        this.p2=npc;
+        this.perceiver1=player;
+        this.perceiver2=npc;
+    }
+
+    private void initDefaults(){
+        //saFactory = new SpeechActFactory();
+        actHistory = new HashMap<>();
+        actHistory.put(perceiver1, new ArrayList<SpeechAct>());
+        actHistory.put(perceiver2, new ArrayList<SpeechAct>());
     }
 
     private void conversation(){
@@ -43,18 +61,31 @@ public class ConversationModel {
             }
         }*/
     }
+
+    private void doOneRound(SpeechAct act){
+        sendActToPerceiver(perceiver2, act); //Should return an answer or there should be a handle for one
+        SpeechAct answer = null;
+        //answer = perceiver2.getAnswer();
+        sendActToPerceiver(perceiver1, act); //No response should be created here
+    }
+
+    //Handles sending a speechact to a perceiver and record and archive what is needed
+    private void sendActToPerceiver(IPerceiver p, SpeechAct act){
+        actHistory.get(p).add(act);     //Add act to history
+        p.hear(act);
+    }
     
     public Image getPlayerImage(){
-    	return p;
+    	return playerImage;
     }
     public Image getNpcImage(){
-    	return n;
+    	return npcImage;
     }
-    public IPerceiver getIPerceiverPlayer(){
-    	return p1;
+    public IPerceiver getIPerceiver1(){
+    	return perceiver1;
     }
-    public IPerceiver getIperceiverNpc(){
-    	return p2;
+    public IPerceiver getIperceiver2(){
+    	return perceiver2;
     }
 
 }
