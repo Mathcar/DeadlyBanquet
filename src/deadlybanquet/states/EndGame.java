77948837c40660cc5f92661a0dcea2ef3ba7c3ad
GustavAxelsson <gustav.93@live.se>
2016-05-03 15:23:47 +0200

    Made the endGame work after you have a conversation

--- src/deadlybanquet/states/EndGame.java
+++ src/deadlybanquet/states/EndGame.java
@@ -23,13 +23,12 @@ public class EndGame extends BasicGameState {
 		text1 = new TextField(gc, gc.getDefaultFont(), 250, 200 ,96,20);
 		text1.setBackgroundColor(Color.white);
 		text1.setAcceptingInput(true);
-		text1.setTextColor(Color.black);
+		text1.setTextColor(Color.white);
 		text1.setMaxLength(10);
 		text1.setText("");
 	}
 	
 	public void render(GameContainer gc, StateBasedGame s, Graphics g) throws SlickException {
-
 		if(wonGame == true){
 			g.drawString("Congratulation you were right!",100,200);
 			g.drawString("Press E to exit.",100,300);
@@ -39,28 +38,39 @@ public class EndGame extends BasicGameState {
 			g.drawString("Press E to exit.",100,300);
 		}
 		else{
-			g.drawString("Write the name of the murderer and press enter",100,60);
-			text1.render(gc, g);
+			//g.drawString("Write the name of the murderer and press enter",100,60);
+			//text1.render(gc, g);
+			g.drawString("Who is the murderer?",100,60);
+			g.drawString("1. Aragon",100,75);
+			g.drawString("2. Daisy",100,90);
+			g.drawString("3. Cindy",100,105);
+			g.drawString("4. Frido",100,120);
+			g.drawString("5. BURT",100,135);
+			g.drawString("6. Sandy",100,150);
 		}
 	}
 	
 	public void update(GameContainer gc, StateBasedGame s, int arg2) throws SlickException {
+		
 		if(!wonGame && !lostGame){
-			if(gc.getInput().isKeyPressed(Input.KEY_ENTER)){
-				System.out.println(text1.getText());
-				if(text1.getText().equals("BURT")){
+			if(gc.getInput().isKeyPressed(Input.KEY_Q)){
+				s.enterState(States.game);
+			}
+			if(gc.getInput().isKeyPressed(Input.KEY_5)){
 					wonGame = true;
 				}
-				else{
+				else if(gc.getInput().isKeyPressed(Input.KEY_1) || gc.getInput().isKeyPressed(Input.KEY_2)
+				|| gc.getInput().isKeyPressed(Input.KEY_3) || gc.getInput().isKeyPressed(Input.KEY_4)
+				|| gc.getInput().isKeyPressed(Input.KEY_6)){
 					lostGame = true;
 				}
-			}
 		}
 		else{
 			if(gc.getInput().isKeyPressed(Input.KEY_E)){
 				gc.exit();
 			}
 		}
+		gc.getInput().clearKeyPressedRecord();
 	}
 	
 	public int getID() {
